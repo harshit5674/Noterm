7de85cfcf9e41066429634802d93563bfb8bd8cb
Harshit Verma <harshit@Harshits-MacBook-Air.local>
2023-01-09 20:19:37 +0530

    Initialized

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "${bold}Thank you for downloading noterm."
+echo "${bold}The professional notetaker for terminal."
+echo "${bold}..................Initializing noterm................."
+
+add=$HOME/.
+
+exit 0
